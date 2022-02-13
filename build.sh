82ef04b6a1cc760c57adcb45b576db25003f36d2
moritzmhmk <moritzmhmk@gmail.com>
2022-02-13 13:17:12 +0100

    add web specific entry point

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..)
-(cd build && emcmake cmake ..)
+
+(cd build && emcmake cmake -DBUILD_WEB=OFF ..)
 (cd build && emmake make VERBOSE=1 -j ${nprocs})
 cp ./build/src/charlsjs.js ./dist
 cp ./build/src/charlsjs.wasm ./dist
+
+(cd build && emcmake cmake -DBUILD_WEB=ON ..)
+(cd build && emmake make VERBOSE=1 -j ${nprocs})
+cp ./build/src/charlsjs.js ./dist/charlsjs.web.js
+
 (cd test/node; npm run test)
